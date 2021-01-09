9cfbbd000d50a6abf9f412c91ad4dcf2ae2c5f6e
Sam Callahan <callahan.l.sam@gmail.com>
2021-01-08 18:37:28 -0600

    Continued working on acquire file. Nearly able to get clue data from any given episode.

--- acquire.py
+++ acquire.py
@@ -25,15 +25,43 @@ def get_episode_urls(season_url):
         episodes.append(episode.find('a').get('href'))
     return episodes
 
+def get_episode_categories(episode_soup):
+    soups = episode_soup.find_all('td', class_="category_name")
+    categories = []
+    for i in soups:
+        if i.string is not None:
+            categories.append(i.string)
+        else:
+            category = ''
+            for element in i.contents:
+                if element.string is not None:
+                    category += element.string
+            categories.append(category)
+    return categories
+
 def get_episode_clues(episode_url):
     response = get(episode_url)
     soup = BeautifulSoup(response.content, 'html.parser')
-    
+    categories = get_episode_categories(soup)
+
     clues = []
+    correct_responses = []
+    correct_response_soups = [] 
+
+    for tag in soup.find_all('div', onmouseover=True): 
+        correct_response_soups.append(tag['onmouseover']) 
     # classes: correct_response, clue_text
+
+    for soup_string in correct_response_soups:
+        correct_response_soup = BeautifulSoup(soup_string)
+        correct_responses.append(correct_response_soup.find('em').string)
     # id: clue_[category_number]_[clue_number]
-    for 
-    
+
+    for tag in soup.find_all(class_='clue_text'):
+        clues.append(tag.string)
+
+    return categories, clues, correct_responses
+
 def get_clues():
     seasons = get_season_urls()
 
