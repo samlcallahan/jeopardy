13c9a151e66f54b94c5104af36306f326f61cca0
Sam Callahan <callahan.l.sam@gmail.com>
2021-08-01 22:29:06 -0500

    Wrote some notes for further work

--- prepare.py
+++
@@ -1,7 +0,0 @@
-'''
-should probably normalize answer before wiki searching them
-write separate csvs for each season so threading actually works
-episode -> episode #, date
-sort by date
-NLP processing
-'''
